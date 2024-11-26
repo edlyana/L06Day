ae6342ef953e79e7f65018279e47cdcf73aeac45
NurEdlyana <23016282@myrp.edu.sg>
2024-11-26 14:43:21 +0800

    Updated codes

---
+++ Data.js
@@ -0,0 +1,15 @@
+const datasource = [
+    {data:[
+            {key: 'a'}, //[0][0]
+            {key: 'e'}, //[0][1]
+        ],
+        title:"Vowels", bgcolor:"skyblue"},
+    {data:[
+            {key: 'b'}, //[1][0]
+            {key: 'c'}, //[1][1]
+            {key: 'd'}, //[1][2]
+        ],
+        title:"Consonants", bgcolor:"khaki"}
+];
+
+export {datasource};
